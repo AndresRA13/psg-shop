2d98fb566e6562f19ce61e0b4c4eae4f8ccc36d5
Andres Rojas <rojasaguileraandres13@gmail.com>
2025-10-21 09:34:45 -0500

    cambios en el modal de la review del adminpanel

--- src/assets/pages/ModernAdminDashboard.jsx
+++ src/assets/pages/ModernAdminDashboard.jsx
@@ -1851,7 +1851,7 @@ const ModernAdminDashboard = () => {
         <div className="fixed inset-0 z-50 overflow-y-auto">
           <div className="flex items-end justify-center min-h-screen px-4 pt-4 pb-20 text-center sm:block sm:p-0">
             <div 
-              className="fixed inset-0 transition-opacity bg-black bg-opacity-50" 
+              className="fixed transition-opacity bg-opacity-50" 
               aria-hidden="true"
               onClick={() => setReviewModalOpen(false)}
             ></div>
